aa88f3a33f205a869607be3da1be5cd0d188d9c7
小針寛乃 <kobarihirono@kobarihiroshinonoMacBook-Pro.local>
2024-04-02 13:45:06 +0900

    [new]PC版スタイリングとフォームバリデーションの実装

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,42 +1,48 @@
 $(document).ready(function () {
   // フォームのバリデーションルールとメッセージを設定
-  $("#form").validate({
+  $("#js-form").validate({
     rules: {
-      user_name: {
+      field_7131055_sei: {
         required: true,
-        maxlength: 100,
+        maxlength: 50,
       },
-      user_email: {
+      field_7131055_mei: {
+        required: true,
+        maxlength: 50,
+      },
+      field_7131056: {
         required: true,
         email: true,
         maxlength: 254,
       },
-      user_message: {
+      field_7131065: {
+        required: true,
         maxlength: 1000,
       },
     },
     messages: {
-      user_name: {
-        required: "ご担当者名を入力してください",
-        maxlength: "100文字以内で入力してください",
+      field_7131055_sei: {
+        required: "姓を入力してください",
+        maxlength: "50文字以内で入力してください",
       },
-      user_email: {
+      field_7131055_mei: {
+        required: "名を入力してください",
+        maxlength: "50文字以内で入力してください",
+      },
+      field_7131056: {
         required: "メールアドレスを入力してください",
         email: "正しい形式で入力してください",
         maxlength: "254文字以内で入力してください",
       },
-      user_message: {
+      field_7131065: {
+        required: "お問い合わせ内容を入力してください",
         maxlength: "1000文字以内で入力してください",
       },
     },
     errorElement: "div",
     errorPlacement: function (error, element) {
       error.addClass("validate-feedback");
-      if (element.attr("name") === "inquiry_type") {
-        error.insertAfter(".p-contact__form-list-item-radio");
-      } else {
-        error.insertAfter(element);
-      }
+      error.insertAfter(element);
     },
   });
 });
